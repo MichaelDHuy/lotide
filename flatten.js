2a8099d6e2301029a9e91008b0064b80a7e57107
MichaelDHuy <dachuy87@email.com>
2022-09-21 16:35:46 +0000

    Created and added codes

---
+++ flatten.js
@@ -0,0 +1,37 @@
+const assertArrayEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  }
+  if (actual !== expected) {
+    console.log("Assertion Failed: " + " " + actual + " !== " + expected);
+  }
+  };
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+      }
+      return true;
+    } 
+  }     
+
+  const flatten = function (arr) {
+    let finalOutput = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        for (let j = 0; j < arr[i].length; j++) {
+          finalOutput.push(arr[i][j]);
+        }
+      } else {
+        finalOutput.push(arr[i]);
+      }
+    }
+    return finalOutput;
+  };
+  
+  console.log(flatten([1, 2, [3, 4], 5, [6]]));
