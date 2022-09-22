207141bc05f393df6dcd820f557babd49d913f0a
MichaelDHuy <dachuy87@email.com>
2022-09-22 00:41:24 +0000

    Created and added codes

---
+++ countLetters.js
@@ -0,0 +1,15 @@
+const countLetters = function(letters) {
+  letters = letters.split(" ").join("");
+  const result = {};
+  for (const letter of letters) {
+if (result[letter]) {
+    result[letter] += 1;
+  } 
+  else {
+    result[letter] = 1;
+  }
+}
+  return result;
+};
+
+console.log(countLetters("lighthouse in the house"));
