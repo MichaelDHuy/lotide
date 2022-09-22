d2521e0af678797925cc77b5dfcbc36e4f052775
MichaelDHuy <dachuy87@email.com>
2022-09-22 06:35:18 +0000

    Created and Added codes

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {   
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  }
+  if (actual !== expected) {
+    console.log("Assertion Failed: " + " " + actual + " !== " + expected);
+  }
+};
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function(selections, show) {
+  for (let selection in selections) {
+    if (selections[selection] === show) {
+     return selection;
+    } 
+    if (selections[selection] !== show) {
+      undefined; 
+    }
+  } 
+}
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
