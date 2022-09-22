33b7ca74b616bd9d690a9ac98f2b2198f6bad4ab
MichaelDHuy <dachuy87@email.com>
2022-09-22 02:04:09 +0000

    Created and added code

---
+++ letterPositions.js
@@ -0,0 +1,42 @@
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] === 0) {
+      continue;
+    }
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i)
+    }
+    else {
+    results[sentence[i]] = [];
+    results[sentence[i]].push(i);
+  }
+  }
+  // logic to update results here
+  return results;
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+      }
+      return true;
+    } 
+  }  
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  }
+  if (!eqArrays(actual, expected)) {
+    console.log("Assertion Failed: " + " " + actual + " !== " + expected);
+  }
+  };
+  assertArraysEqual(letterPositions("hello").e, [1]);
+  assertArraysEqual(letterPositions('lighthouse in the house').e, [9, 16, 22]);
