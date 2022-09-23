384cc5ab6f1fce10937318b3a45e616531e01d55
MichaelDHuy <dachuy87@email.com>
2022-09-23 05:20:35 +0000

    Created and codes added

---
+++ findKey.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  }
+  if (actual !== expected) {
+    console.log("Assertion Failed: " + " " + actual + " !== " + expected);
+  }
+  };
+  const findKey = function(objects, callback) {
+  for (let object in objects) {
+    if(callback(objects[object])) {
+    return object;
+    } 
+  }
+  }
+  const resultX = findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 3) // => "noma"
+  console.log(resultX);
+
+  assertEqual(resultX, "Akaleri");
