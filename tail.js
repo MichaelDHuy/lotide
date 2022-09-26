9a2cb61ce4d52822981053ff1235d2c47b5742df
MichaelDHuy <dachuy87@email.com>
2022-09-26 22:16:20 +0000

    Changed module

--- tail.js
+++ tail.js
@@ -1,15 +1,9 @@
-const actual = ["Hello", "Lighthouse", "Labs"];
-const result = function(actual) {
-let resultFace = actual.slice(1);
+
+const tail = function(array) {
+let resultFace = array.slice(1);
 return resultFace;
-} 
-const assertEqual = function(actual, expected) {   
-  if (actual === expected) {
-    console.log("Assertion Passed: " + result + " === " + expected);
-  }
-  if (actual !== expected) {
-    console.log("Assertion Failed: " + " " + result + " !== " + expected);
-  }
 };
-assertEqual(result(actual), ["Lighthouse", "Labs"]);
-assertEqual(actual.length, 3);
+
+module.exports = tail;
+
+
