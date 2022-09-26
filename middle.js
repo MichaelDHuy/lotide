7717375d24fe8ced47be93479da11692628a295f
MichaelDHuy <dachuy87@email.com>
2022-09-26 23:21:24 +0000

    Added module exports

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const assertArrayEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("Assertion Passed: " + actual + " === " + expected);
-  }
-  if (actual !== expected) {
-    console.log("Assertion Failed: " + " " + actual + " !== " + expected);
-  }
-  };
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-      }
-      return true;
-    } 
-  }     
-
   const middle = function(array) {
     let newArray = [];
     let startIndex = Math.floor(array.length/2);
@@ -36,16 +14,4 @@ const eqArrays = function(array1, array2) {
     return newArray;
   }
 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
-
-assertArrayEqual(middle([1]), []);
-assertArrayEqual(middle([1, 2]), []);
-
-assertArrayEqual(middle([1, 2, 3]), [2]);
-assertArrayEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-assertArrayEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArrayEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
