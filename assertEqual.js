34a60060192e8693ab88f2c0a54bc5ff1ee88352
MichaelDHuy <dachuy87@email.com>
2022-09-26 21:39:25 +0000

    Edited code

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,4 @@
-const headArray = function(actual) {
-  return actual[0];
-}
+
 const assertEqual = function(actual, expected) {   
   if (actual === expected) {
     console.log("Assertion Passed: " + actual + " === " + expected);
@@ -9,5 +7,5 @@ const assertEqual = function(actual, expected) {
     console.log("Assertion Failed: " + " " + actual + " !== " + expected);
   }
 };
-assertEqual(headArray([5,6,7]), 5);
-assertEqual(headArray(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = assertEqual;
