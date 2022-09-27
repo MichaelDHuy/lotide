8978405b0303d5434dd498573feec1d1e98db9bf
MichaelDHuy <dachuy87@email.com>
2022-09-27 04:09:21 +0000

    Add chai mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,8 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
-const actual = ["Hello", "Lighthouse", "Labs"];
 
-assertEqual(tail(actual), ["Lighthouse", "Labs"]);
-assertEqual(actual.length, 3);
+describe("#tail", () => {
+it("returns 'Lighthouse', 'Labs' for ['Hello', 'Lighthouse', 'Labs']", () => {
+  assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+})
+});
