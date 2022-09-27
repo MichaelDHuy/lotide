d8bf62b0ad20623cf7cbc81e7a39fc7b3c1df419
MichaelDHuy <dachuy87@email.com>
2022-09-27 06:13:01 +0000

    Module export added

--- takeUntil.js
+++ takeUntil.js
@@ -19,3 +19,17 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
+const assertEqual = function(actual, expected) {   
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  }
+  if (actual !== expected) {
+    console.log("Assertion Failed: " + " " + actual + " !== " + expected);
+  }
+};
+
+assertEqual(results1, [1, 2, 5, 7, 2]);
+assertEqual(results2, ["I've", "been", "Hollywood"]);
+
+module.exports = takeUntil.js;
