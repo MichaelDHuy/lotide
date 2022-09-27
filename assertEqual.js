ef782881cc2f4561295f456c8b7c222c45399dda
MichaelDHuy <dachuy87@email.com>
2022-09-27 06:09:43 +0000

    Module export added

--- assertEqual.js
+++ assertEqual.js
@@ -2,6 +2,7 @@
 const assertEqual = function(actual, expected) {   
   if (actual === expected) {
     console.log("Assertion Passed: " + actual + " === " + expected);
+    return true;
   }
   if (actual !== expected) {
     console.log("Assertion Failed: " + " " + actual + " !== " + expected);
